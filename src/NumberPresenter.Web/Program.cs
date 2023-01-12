be7c0284e3e92812bb8870d61fb62524331cac67
Jonathan Miles <jonathan.miles@muddyboots.com>
2023-01-12 14:02:03 +0000

    make application into web application

--- src/NumberPresenter.Web/Program.cs
+++ src/NumberPresenter.Web/Program.cs
@@ -1,6 +1,22 @@
+using NumberPresenter.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<INumberService, NumberService>();
+builder.Services.AddScoped<IRomanService, RomanService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}");
+});
 
 app.Run();
