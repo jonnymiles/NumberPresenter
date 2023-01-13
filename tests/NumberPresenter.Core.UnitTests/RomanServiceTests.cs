94a0ef5cd2dbe98144a8b2a8cd4260c5ba32e810
Jonathan Miles <jonathan.miles@muddyboots.com>
2023-01-13 08:31:50 +0000

    unit tests

--- tests/NumberPresenter.Core.UnitTests/RomanServiceTests.cs
+++ tests/NumberPresenter.Core.UnitTests/RomanServiceTests.cs
@@ -5,12 +5,64 @@ namespace NumberPresenter.Core.UnitTests
     [TestClass]
     public class RomanServiceTests
     {
-        [TestMethod]
-        public void SmokeTest()
+        [DataTestMethod]
+        [DataRow(49, "XLIX")]
+        [DataRow(426, "CDXXVI")]
+        [DataRow(2000, "MM")]
+        [DataRow(606, "DCVI")]
+        [DataRow(1, "I")]
+        public void NumericToRoman_Int_Tests(int numeric, string numeral)
         {
             var subject = GetSubject();
 
-            var result = subject.NumericToRoman(1234);
+            var result = subject.NumericToRoman(numeric);
+
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(numeral, result.Result);
+        }
+
+        [DataTestMethod]
+        [DataRow("49", "XLIX")]
+        [DataRow("426", "CDXXVI")]
+        [DataRow("2000", "MM")]
+        [DataRow("606", "DCVI")]
+        [DataRow("1", "I")]
+        public void NumericToRoman_String_Tests(string numeric, string numeral)
+        {
+            var subject = GetSubject();
+
+            var result = subject.NumericToRoman(numeric);
+
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(numeral, result.Result);
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow("  ")]
+        public void NumericToRoman_String_NoStringReturnsError(string numeric)
+        {
+            var subject = GetSubject();
+
+            var result = subject.NumericToRoman(numeric);
+
+            Assert.IsFalse(result.Success);
+            Assert.AreEqual("Please enter a value", result.ErrorMessage);
+        }
+
+        [DataTestMethod]
+        [DataRow("onetwothree")]
+        [DataRow("123hello")]
+        public void NumericToRoman_String_NonNumberReturnsError(string numeric)
+        {
+            var subject = GetSubject();
+
+            var result = subject.NumericToRoman(numeric);
+
+            Assert.IsFalse(result.Success);
+            Assert.AreEqual("Please enter a valid number", result.ErrorMessage);
         }
 
         private RomanService GetSubject()
